ff74c73fbc02fc171fb662f410add4362236fe2b
slopezxpez <smarcianolopez@gmail.com>
2023-09-21 12:15:21 -0500

    SSH Key deployment, now testing Git Updater

---
+++ plugins/git-updater/src/Git_Updater/Add_Ons.php
@@ -0,0 +1,323 @@
+<?php
+/**
+ * Git Updater
+ *
+ * @author   Andy Fragen
+ * @license  MIT
+ * @link     https://github.com/afragen/git-updater
+ * @package  git-updater
+ */
+
+namespace Fragen\Git_Updater;
+
+use Fragen\Singleton;
+use Fragen\Git_Updater\Traits\GU_Trait;
+
+/**
+ * Class Add_Ons
+ */
+class Add_Ons {
+	use GU_Trait;
+
+	/**
+	 * Holds free add-on config data.
+	 *
+	 * @var array
+	 */
+	protected $addon;
+
+	/**
+	 * Holds premium add-on config data.
+	 *
+	 * @var array
+	 */
+	protected $premium_addon;
+
+	/**
+	 *  Holds URL for form action.
+	 *
+	 * @var string
+	 */
+	protected $action;
+
+	/**
+	 * Add_Ons constructor.
+	 */
+	public function __construct() {
+		$this->addon = $this->load_addon_config();
+		validate_active_plugins();
+	}
+
+	/**
+	 * Load add-on config data.
+	 *
+	 * @return array
+	 */
+	protected function load_addon_config() {
+		$config = [
+			'gist'      => [
+				[
+					'name'        => __( 'Git Updater - Gist', 'git-updater' ),
+					'author'      => __( 'Andy Fragen' ),
+					'description' => __( 'Add GitHub Gist hosted repositories to the Git Updater plugin.', 'git-updater' ),
+					'host'        => 'github',
+					'slug'        => 'git-updater-gist/git-updater-gist.php',
+					'uri'         => 'afragen/git-updater-gist',
+					'branch'      => 'main',
+					'required'    => true,
+					'api'         => 'gist',
+				],
+			],
+			'bitbucket' => [
+				[
+					'name'        => __( 'Git Updater - Bitbucket', 'git-updater' ),
+					'author'      => __( 'Andy Fragen' ),
+					'description' => __( 'Add Bitbucket and Bitbucket Server hosted repositories to the Git Updater plugin.', 'git-updater' ),
+					'host'        => 'github',
+					'slug'        => 'git-updater-bitbucket/git-updater-bitbucket.php',
+					'uri'         => 'afragen/git-updater-bitbucket',
+					'branch'      => 'main',
+					'required'    => true,
+					'api'         => 'bitbucket',
+				],
+			],
+			'gitlab'    => [
+				[
+					'name'        => __( 'Git Updater - GitLab', 'git-updater' ),
+					'author'      => __( 'Andy Fragen' ),
+					'description' => __( 'Add GitLab hosted repositories to the Git Updater plugin.', 'git-updater' ),
+					'host'        => 'github',
+					'slug'        => 'git-updater-gitlab/git-updater-gitlab.php',
+					'uri'         => 'afragen/git-updater-gitlab',
+					'branch'      => 'main',
+					'required'    => true,
+					'api'         => 'gitlab',
+				],
+			],
+			'gitea'     => [
+				[
+					'name'        => __( 'Git Updater - Gitea', 'git-updater' ),
+					'author'      => __( 'Andy Fragen' ),
+					'description' => __( 'Add Gitea hosted repositories to the Git Updater plugin.', 'git-updater' ),
+					'host'        => 'github',
+					'slug'        => 'git-updater-gitea/git-updater-gitea.php',
+					'uri'         => 'afragen/git-updater-gitea',
+					'branch'      => 'main',
+					'required'    => true,
+					'api'         => 'gitea',
+				],
+			],
+		];
+
+		return $config;
+	}
+
+	/**
+	 * Load needed action/filter hooks.
+	 */
+	public function load_hooks() {
+		add_action( 'admin_init', [ $this, 'addons_page_init' ] );
+
+		$this->add_settings_tabs();
+	}
+
+	/**
+	 * Adds Remote Management tab to Settings page.
+	 */
+	public function add_settings_tabs() {
+		$install_tabs = [ 'git_updater_addons' => esc_html__( 'API Add-Ons', 'git-updater' ) ];
+		add_filter(
+			'gu_add_settings_tabs',
+			function ( $tabs ) use ( $install_tabs ) {
+				return array_merge( $tabs, $install_tabs );
+			}
+		);
+		add_filter(
+			'gu_add_admin_page',
+			function ( $tab, $action ) {
+				$this->add_admin_page( $tab, $action );
+			},
+			10,
+			2
+		);
+	}
+
+	/**
+	 * Add Settings page data via action hook.
+	 *
+	 * @uses 'gu_add_admin_page' action hook
+	 *
+	 * @param string $tab    Tab name.
+	 * @param string $action Form action.
+	 */
+	public function add_admin_page( $tab, $action ) {
+		if ( 'git_updater_addons' === $tab ) {
+			$this->action = add_query_arg( 'tab', $tab, $action );
+			$this->admin_page_notices();
+			do_settings_sections( 'git_updater_addons_settings' );
+		}
+	}
+
+	/**
+	 * Display appropriate notice for Remote Management page action.
+	 */
+	private function admin_page_notices() {
+		if ( ! isset( $_POST['_wpnonce'] ) || ! wp_verify_nonce( sanitize_key( wp_unslash( $_POST['_wpnonce'] ) ), 'gu_settings' ) ) {
+			return;
+		}
+		$display = isset( $_POST['install_api_plugin'] ) && '1' === $_POST['install_api_plugin'];
+		if ( $display ) {
+			echo '<div class="updated"><p>';
+			esc_html_e( 'Git Updater API plugin installed.', 'git-updater' );
+			echo '</p></div>';
+		}
+	}
+
+	/**
+	 * Settings for Add Ons.
+	 */
+	public function addons_page_init() {
+		register_setting(
+			'git_updater_addons',
+			'git_updater_addons_settings',
+			[ $this, 'sanitize' ]
+		);
+
+		add_settings_section(
+			'addons',
+			esc_html__( 'API Add-Ons', 'git-updater' ),
+			[ $this, 'insert_cards' ],
+			'git_updater_addons_settings'
+		);
+	}
+
+	/**
+	 * Some method to insert cards for API plugin installation.
+	 *
+	 * @return void
+	 */
+	public function insert_cards() {
+		echo '<p>';
+		esc_html_e( 'Install additional API plugins.', 'git-updater' );
+		echo '</p>';
+		echo '<div class="wp-list-table widefat plugin-install">';
+		foreach ( $this->addon as $addon ) {
+			$addon = \array_pop( $addon );
+			$this->make_card( 'free', $addon );
+		}
+		echo '</div>';
+		echo '<div style="clear:both;"></div>';
+	}
+
+	/**
+	 * Install Git Updater API plugins.
+	 *
+	 * @uses afragen/wp-dependency-installer
+	 *
+	 * @return bool
+	 */
+	public function install_api_plugin() {
+		$config = false;
+		// phpcs:disable WordPress.Security.NonceVerification.Recommended
+		if ( isset( $_GET['install_api_plugin'] ) ) {
+
+			// Redirect back to the Add-Ons.
+			$_POST                     = $_REQUEST;
+			$_POST['_wp_http_referer'] = isset( $_SERVER['HTTP_REFERER'] ) ? esc_url_raw( wp_unslash( $_SERVER['HTTP_REFERER'] ) ) : null;
+
+			switch ( $_GET['install_api_plugin'] ) {
+			//phpcs:enable
+				case 'gist':
+					$config = $this->addon['gist'];
+					break;
+				case 'bitbucket':
+					$config = $this->addon['bitbucket'];
+					break;
+				case 'gitlab':
+					$config = $this->addon['gitlab'];
+					break;
+				case 'gitea':
+					$config = $this->addon['gitea'];
+					break;
+			}
+
+			if ( $config ) {
+				\WP_Dependency_Installer::instance()->register( $config )->admin_init();
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Create Add-on card.
+	 *
+	 * @param string $type   Type of addon, free|premium.
+	 * @param array  $config Array of add-on config data.
+	 *
+	 * @return void
+	 */
+	public function make_card( $type, $config ) {
+		if ( 'free' === $type ) {
+			$config['repo']                 = basename( $config['uri'] );
+			$config['owner']                = dirname( $config['uri'] );
+			$repo_api                       = Singleton::get_instance( 'API\API', $this )->get_repo_api( 'github' );
+			$repo_api->type->owner          = $config['owner'];
+			$repo_api->type->slug           = $config['repo'];
+			$repo_api->type->branch         = $config['branch'];
+			$repo_api->type->type           = 'plugin';
+			$repo_api->type->git            = 'github';
+			$repo_api->type->enterprise     = false;
+			$repo_api->type->enterprise_api = false;
+		}
+
+		$plugin_icon_url = \plugin_dir_url( dirname( __DIR__ ) ) . 'assets/icon.svg';
+
+		?>
+		<div class="git-updater plugin-card plugin-card-<?php echo sanitize_html_class( $config['repo'] ); ?>">
+			<div class="plugin-card-top">
+				<div class="name column-name">
+					<h3>
+					<?php echo esc_html( $config['name'] ); ?>
+					<img src="<?php echo esc_attr( $plugin_icon_url ); ?>" class="plugin-icon" alt="" />
+					</h3>
+				</div>
+				<div class="desc column-description">
+					<p><?php echo esc_html( $config['description'] ); ?></p>
+					<p class="authors"><?php echo esc_html( $config['author'] ); ?></p>
+				</div>
+			</div>
+			<div class="plugin-card-bottom">
+				<?php
+				if ( 'free' === $type ) {
+					$this->free_button( $config );
+				}
+				if ( 'premium' === $type ) {
+					$this->premium_button( $config );
+				}
+				?>
+			</div>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Get button for free add-on card.
+	 *
+	 * @param array $config Array of plugin data.
+	 *
+	 * @return void
+	 */
+	private function free_button( $config ) {
+		$install_api = add_query_arg( [ 'install_api_plugin' => $config['api'] ], $this->action );
+		if ( \is_plugin_active( $config['slug'] ) ) {
+			submit_button( esc_html__( 'Install & Activate', 'git-updater' ), 'disabled' );
+		} else {
+			?>
+			<form class="settings no-sub-tabs" method="post" action="<?php echo esc_attr( $install_api ); ?>">
+				<?php submit_button( esc_html__( 'Install & Activate', 'git-updater' ) ); ?>
+			</form>
+			<?php
+		}
+	}
+}
---
+++ plugins/git-updater/src/Git_Updater/Bootstrap.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ * Git Updater
+ *
+ * @author   Andy Fragen
+ * @license  MIT
+ * @link     https://github.com/afragen/git-updater
+ * @package  git-updater
+ */
+
+namespace Fragen\Git_Updater;
+
+use Fragen\Git_Updater\Additions\Bootstrap as Additions_Bootstrap;
+use Fragen\Git_Updater\REST\REST_API;
+use Fragen\Git_Updater\Traits\GU_Trait;
+
+/*
+ * Exit if called directly.
+ */
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Load textdomain.
+add_action(
+	'init',
+	function () {
+		load_plugin_textdomain( 'git-updater' );
+	}
+);
+
+/**
+ * Class Bootstrap
+ */
+class Bootstrap {
+	use GU_Trait;
+
+	/**
+	 * Holds main plugin file.
+	 *
+	 * @var string
+	 */
+	protected $file;
+
+	/**
+	 * Holds main plugin directory.
+	 *
+	 * @var string
+	 */
+	protected $dir;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param  string $file Main plugin file.
+	 * @return void
+	 */
+	public function __construct( $file ) {
+		$this->file = $file;
+		$this->dir  = dirname( $file );
+	}
+
+	/**
+	 * Deactivate plugin and die as composer autoloader not loaded.
+	 *
+	 * @return void
+	 */
+	public function deactivate_die() {
+		require_once ABSPATH . 'wp-admin/includes/plugin.php';
+		\deactivate_plugins( plugin_basename( $this->file ) );
+
+		$message = sprintf(
+			/* translators: %1: opening tag, %2: closing tag */
+			__( 'Git Updater is missing required composer dependencies. %1$sLearn more.%2$s', 'git-updater' ),
+			'<a href="https://github.com/afragen/git-updater/wiki/Installation" target="_blank" rel="noreferrer">',
+			'</a>'
+		);
+
+		wp_die( wp_kses_post( $message ) );
+	}
+
+	/**
+	 * Run the bootstrap.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		if ( ! $this->check_requirements() ) {
+			return;
+		}
+
+		register_deactivation_hook( $this->file, [ $this, 'remove_cron_events' ] );
+		require_once ABSPATH . 'wp-admin/includes/plugin.php';
+		deactivate_plugins( [ 'git-updater-pro/git-updater-pro.php', 'git-updater-additions/git-updater-additions.php' ] );
+
+		require_once __DIR__ . '/Shim.php';
+		( new GU_Freemius() )->init();
+		( new REST_API() )->load_hooks();
+		( new Additions_Bootstrap( $this->file ) )->run();
+		( new Init() )->run();
+		( new Messages() )->create_error_message( 'get_license' );
+
+		// Initialize time dissmissible admin notices.
+		new \WP_Dismiss_Notice();
+	}
+
+	/**
+	 * Check PHP requirements and deactivate plugin if not met.
+	 *
+	 * @return void|bool
+	 */
+	public function check_requirements() {
+		if ( version_compare( phpversion(), '7.2', '<=' ) ) {
+			add_action(
+				'admin_init',
+				function () {
+					echo '<div class="error notice is-dismissible"><p>';
+					printf(
+						/* translators: 1: minimum PHP version required */
+						wp_kses_post( __( 'Git Updater cannot run on PHP versions older than %1$s.', 'git-updater' ) ),
+						'7.2'
+					);
+					echo '</p></div>';
+					\deactivate_plugins( plugin_basename( $this->file ) );
+				}
+			);
+
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Remove scheduled cron events on deactivation.
+	 *
+	 * @return void
+	 */
+	public function remove_cron_events() {
+		$crons = [ 'gu_get_remote_plugin', 'gu_get_remote_theme' ];
+		foreach ( $crons as $cron ) {
+			$timestamp = \wp_next_scheduled( $cron );
+			\wp_unschedule_event( $timestamp, $cron );
+		}
+	}
+
+	/**
+	 * Rename on activation.
+	 *
+	 * Correctly renames the slug when Git Updater is installed
+	 * via FTP or from plugin upload.
+	 *
+	 * Set current branch to `develop` if appropriate.
+	 *
+	 * `rename()` causes activation to fail.
+	 *
+	 * @return void|bool
+	 */
+	public function rename_on_activation() {
+		global $wp_filesystem;
+
+		if ( ! $wp_filesystem ) {
+			require_once ABSPATH . '/wp-admin/includes/file.php';
+			WP_Filesystem();
+		}
+
+		// Exit if coming from webhook.
+		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		if ( isset( $_GET['plugin'], $_GET['webhook_source'] ) && 'git-updater' === $_GET['plugin'] ) {
+			return;
+		}
+
+		$plugin_dir = trailingslashit( WP_PLUGIN_DIR );
+		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		$slug     = isset( $_GET['plugin'] ) ? sanitize_text_field( wp_unslash( $_GET['plugin'] ) ) : false;
+		$exploded = explode( '-', dirname( $slug ) );
+
+		if ( in_array( 'develop', $exploded, true ) ) {
+			$options = $this->get_class_vars( 'Base', 'options' );
+			update_site_option( 'git_updater', array_merge( $options, [ 'current_branch_git-updater' => 'develop' ] ) );
+		}
+
+		if ( $slug && 'git-updater/git-updater.php' !== $slug ) {
+			require_once __DIR__ . '/Shim.php';
+			$result = move_dir( $plugin_dir . dirname( $slug ), $plugin_dir . 'git-updater', true );
+			if ( \is_wp_error( $result ) ) {
+				return $result;
+			}
+		}
+	}
+}
